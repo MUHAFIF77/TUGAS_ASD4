69f7e2a6a5fc32f74b7850ccfb38a0aade497fde
MUHAFIF77 <43754532+MUHAFIF77@users.noreply.github.com>
2018-11-15 15:19:54 +0800

    Create AlgoritmaInsertionSort.java

---
+++ AlgoritmaInsertionSort.java
@@ -0,0 +1,41 @@
+// Java program for implementation of Insertion Sort 
+class Algoritma_Insertion_Sort
+{ 
+public class AlgoritmaInsertionSort {
+
+	// Java program for implementation of Insertion Sort 
+	    /*Function to sort array using insertion sort*/
+		  
+	    /* A utility function to print array of size n*/
+	    public void printArray(int arr[]) 
+	    { 
+	        int n = arr.length; 
+	        for (int i=0; i<n; ++i) 
+	            System.out.print(arr[i] + " "); 
+	  
+	        System.out.println(); 
+	    } 
+	  
+	    // Driver method 
+	    public void main(String args[]) 
+	    {         
+	        int arr[] = {12, 11, 13, 5, 6}; 
+	  
+	        Algoritma_Insertion_Sort ob = new Algoritma_Insertion_Sort();         
+	        ob.sort(arr); 
+	          
+	        printArray(arr); 
+	    }
+
+		private void sort(int[] arr) {
+			// TODO Auto-generated method stub
+			
+		} 
+	}
+
+public void sort(int[] arr) {
+	// TODO Auto-generated method stub
+	
+}
+
+}
